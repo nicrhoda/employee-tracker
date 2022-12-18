3345b50efe3b5164cb25ef8c3908e331e2ca2fcd
nic <nrhoda14@gmail.com>
2022-12-17 20:18:08 -0600

    added prompts.js to house prompts, added package.json to install packages, and added .gitignore

---
+++ prompts.js
@@ -0,0 +1,15 @@
+const inquirer = require('inquirer');
+const express = require('express');
+
+const mainPrompt = () => {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+        }
+    ])
+}
+
+mainPrompt();
